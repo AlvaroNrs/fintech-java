bb69af29aedec8a5c36f476febc19dc6c6e1c450
Carlos <calpro35@outlook.com>
2024-10-07 16:28:05 -0300

    alteracao da minha senha e feito minhabranch

--- src/main/java/br/com/fiap/factory/ConnectionFactory.java
+++ src/main/java/br/com/fiap/factory/ConnectionFactory.java
@@ -7,8 +7,8 @@ public class ConnectionFactory {
 
     // De acordo com as configurações do Banco de cada um, essas variáveis precisarão ser altaradas para testar a conexão com o banco
     private static final String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-    private static final String USUARIO = "RM556345";
-    private static final String SENHA = "080502";
+    private static final String USUARIO = "RM557504";
+    private static final String SENHA = "290188";
 
     // Método para obter uma conexão com o banco de dados
     public static Connection getConnection() throws SQLException {
